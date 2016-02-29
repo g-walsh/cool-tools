e200e603077cdb3dcca776cde52601f6c05d9c57
Gordon Walsh <gordon.p.walsh@gmail.com>
2016-02-29 18:58:04 +0000

    First Commit
    First commit - filebot subtitle downloading script

---
+++ sub_dl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# A simple bash script to download subtitle files for any files in a directory
+# that currently don't have subtitle files (named exactly the same). This
+# script requires the cli version of the excellent tool Filebot
+# (filebot.net) which is written in java. Please support their excellent work
+# by donating/buying Filebot.
+
+# Started 29.02.2016 by Gordon Walsh (https://github.com/g-walsh)
