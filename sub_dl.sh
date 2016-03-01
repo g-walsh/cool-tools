02674b4c0a409e38d75cef856923fbb6a8368404
Gordon Walsh <gordon.p.walsh@gmail.com>
2016-03-01 15:01:09 +0000

    Initial script commit
    First run at writing the script.
    
    - Get user to define media as the root folder of your media library
    - Create temporary directory
    - Write a list of all subfolders to temporary file and then run filebot for each subfolder and write output to a log file
    
    Not tested yet

--- sub_dl.sh
+++ sub_dl.sh
@@ -6,4 +6,16 @@
 # (filebot.net) which is written in java. Please support their excellent work
 # by donating/buying Filebot.
 
+# To use this script yourself change /path/to/media to your media library.
+
 # Started 29.02.2016 by Gordon Walsh (https://github.com/g-walsh)
+
+media=/path/to/media
+
+mkdir -p /tmp/filebot
+find $media -type d > /tmp/filebot/sub_dl
+
+for line in $(cat /tmp/filebot/sub_dl);
+do
+  filebot -get-missing-subtitles $line >> /tmp/filebot/sub_log;
+done
