734a934912f528956e8c104767464d727e76830d
Gordon Walsh <gordon.p.walsh@gmail.com>
2016-03-17 10:10:40 +0000

    Include whole client.crt
    Having re read the vpn tutorial I decided to include the whole client.crt file rather than just the certificate part.
    
    - Comment out old lines that extract the certificate part of the client.crt file
    - rewrite the echo so account for this

--- ovpngen.sh
+++ ovpngen.sh
@@ -6,15 +6,21 @@
 # client27.key      - client key file
 # client27.crt      - client certificate file
 # client_temp.ovpn  - template .ovpn file
+#
+# Updated to include the entire client .crt not just the certificate part.
 
 name=$1
 touch ./$name.ovpn ./tmp ./lines
 
-cat $name.crt | grep -n '\-----' | grep -o '[0-9]\{2\}' > lines
-line1=$(head -1 lines)
-line2=$(tail -1 lines)
+# This next line extracts the certificate part from the client.crt
+# cat $name.crt | grep -n '\-----' | grep -o '[0-9]\{2\}' > lines
+# line1=$(head -1 lines)
+# line2=$(tail -1 lines)
 
-echo -e "\n<ca>\n $(cat ca.crt)\n</ca>\n\n<cert>\n $(sed -n $(echo $line1),$(echo $line2)p $name.crt)\n</cert>\n\n<key>\n $(cat $name.key)\n</key>" > ./tmp
+# Comment out using lines rather than whole client.crt file
+# echo -e "\n<ca>\n $(cat ca.crt)\n</ca>\n\n<cert>\n $(sed -n $(echo $line1),$(echo $line2)p $name.crt)\n</cert>\n\n<key>\n $(cat $name.key)\n</key>" > ./tmp
+
+echo -e "\n<ca>\n $(cat ca.crt)\n</ca>\n\n<cert>\n $(cat $name.crt)\n</cert>\n\n<key>\n $(cat $name.key)\n</key>" > ./tmp
 
 cat client_temp.ovpn tmp > $name.ovpn
 
