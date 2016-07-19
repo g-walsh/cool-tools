1df61a194ec1dcc5248d8fb71ee6feb154d0fc64
g-walsh <gordon.p.walsh@gmail.com>
2016-07-19 17:20:34 +0100

    Initial commit of script to automate downloading and renaming missing subtitles

---
+++ missingsubs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# A script to download missing subtitle files (using filebot) in english and
+# french. This will also rename english subtitles to .en.srt from .eng.srt for
+# better interpretation by Plex Media Center.
+
+path=$1
+
+filebot -get-subtitles $path --lang en --output srt --encoding utf8 -non-strict
+filebot -get-subtitles $path --lang fr --output srt --encoding utf8 -non-strict
+
+for i in $(find $path -type f | grep '^[^.]*\.srt');
+do
+  echo $i
+done
+
+for i in $(find $path -type f | grep '^[^.]*\.en.srt');
+do
+  echo $i
+done
+
+for i in $(find $path -type f | grep '^[^.]*\.eng.srt');
+do
+  echo $i
+done
+
+for i in $(find $path -type f | grep '^[^.]*\.fr.srt');
+do
+  echo $i
+done
